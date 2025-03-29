c233c3af32d031f6c72617e0df396ff5747bd021
mudachii <emudahi@cs.colman.ac.il>
2025-03-29 18:43:51 +0300

    refactor: comment out response interceptor in apiClient for future implementation

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -8,25 +8,25 @@ const apiClient = axios.create({
   }
 });
 
-apiClient.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async (error) => {
-    const originalRequest = error.config;
-    if (error.response && error.response.data === "Unauthorized") {
-      try {
-        // const accessToken = await refreshTokens();
-        // originalRequest.headers.Authorization = `Bearer ${accessToken}`;
-        originalRequest._retry = true;
-        return apiClient(originalRequest);
-      } catch (refreshError) {
-        return Promise.reject(refreshError);
-      }
-    }
-    return error;
-  }
-);
+// apiClient.interceptors.response.use(
+//   (response) => {
+//     return response;
+//   },
+//   async (error) => {
+//     const originalRequest = error.config;
+//     if (error.response && error.response.data === "Unauthorized") {
+//       try {
+//         // const accessToken = await refreshTokens();
+//         // originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+//         originalRequest._retry = true;
+//         return apiClient(originalRequest);
+//       } catch (refreshError) {
+//         return Promise.reject(refreshError);
+//       }
+//     }
+//     return error;
+//   }
+// );
 
 // async function refreshTokens() {
 //   const refreshResponse = await refresh();
