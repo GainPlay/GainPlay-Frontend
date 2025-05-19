fdcd4ec7502a6a0f013bb630c40825eb86eccd6b
mudachii <emudahi@cs.colman.ac.il>
2025-05-19 23:37:46 +0300

    refactor: integrate user store in AuthPage and ProfilePage for user data management

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -5,9 +5,13 @@ import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import { signup, saveTokens, signin } from "@/services/authService";
 import { useCallback } from "react";
+import { useUserStore } from "@/stores/useUserStore";
+import { FriendshipStatus } from "@/types";
+import { userService } from "@/services/userService";
 
 export default function AuthPage() {
   const navigate = useNavigate();
+  const user = useUserStore();
 
   const handleLogin = useCallback(
     async (e: React.FormEvent<HTMLFormElement>) => {
@@ -21,6 +25,10 @@ export default function AuthPage() {
         const response = await signin(email, password);
         if (response.status === 201 && response.data?.access_token) {
           saveTokens({ accessToken: response.data.access_token });
+      const fetchedUser = await userService.getUserDataByMail(email);
+      //@ts-ignore
+      user.setUser({...fetchedUser, ...fetchedUser.user_settings,...fetchedUser.user_badges,...fetchedUser.user_goals});
+      
           navigate("/home");
         } else {
           if (response.status === 400) {
@@ -51,6 +59,8 @@ export default function AuthPage() {
         if (response.status === 201) {
           if (response.data?.access_token) {
             saveTokens({ accessToken: response.data.access_token });
+            const fetchedUser = await userService.getUserDataByMail(email);
+            user.setUser({...fetchedUser, ...fetchedUser.user_settings,...fetchedUser.user_badges,...fetchedUser.user_goals});
           }
         } else {
           if (response.status === 400) {
