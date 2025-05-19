b086705c4ac3f48b7bc364cca1cb4d98040b70af
mudachii <emudahi@cs.colman.ac.il>
2025-05-19 23:57:45 +0300

    refactor: update userService to use axios for getUserData and improve userStore structure

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,8 +1,15 @@
 import { FrontendUserData } from "@/types";
+import axios from "axios";
 import apiClient from "./apiClient";
 
 export const userService = {
-  getUserData: async (userId: Number): Promise<FrontendUserData> => {
-    return (await apiClient.get(`/users/${userId}`)).data;
+  getUserData: async (userId: number): Promise<FrontendUserData> => {
+    return (await axios.get(`/users/${userId}`)).data;
+  },
+  getUserDataByMail: async (email: string): Promise<FrontendUserData> => {
+    return (await apiClient.get(`/users/${email}/mail`)).data;
+  },
+  updateUser: async (userId: number, userData:any): Promise<FrontendUserData> => {
+    return (await apiClient.post(`/users/${userId}/updateUser`, {userData})).data;
   },
 };
