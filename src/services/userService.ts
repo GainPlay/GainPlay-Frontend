25ef4afc86db564de2f5876d6e5734bdaa83f626
mudachii <emudahi@cs.colman.ac.il>
2025-05-20 00:06:20 +0300

    refactor: replace apiClient with axios for user data retrieval methods

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,16 +1,15 @@
 import { FrontendUserData } from "@/types";
 import axios from "axios";
-import apiClient from "./apiClient";
 
 export const userService = {
   getUserData: async (userId: number): Promise<FrontendUserData> => {
     return (await axios.get(`/users/${userId}`)).data;
   },
   getUserDataByMail: async (email: string): Promise<FrontendUserData> => {
-    return (await apiClient.get(`/users/${email}/mail`)).data;
+    return (await axios.get(`/users/${email}/mail`)).data;
   },
   updateUser: async (userId: number, userData:any): Promise<FrontendUserData> => {
-    return (await apiClient.post(`/users/${userId}/updateUser`, {userData})).data;
+    return (await axios.post(`/users/${userId}/updateUser`, {userData})).data;
   },
 
   updateUserSettings: async (
