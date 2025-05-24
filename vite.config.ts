7f75cabba8466627d116124ca0802145f6f79528
Hila <hila.ohana1910@gmail.com>
2025-05-24 14:15:00 +0300

    fix cors

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,6 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": "http://localhost:3000",
-    },
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
